6b745e48fe98d21b88984926df686b22a6fd6535
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-16 16:51:54 +0300

    added alternative declaration state and added ComponentDidMount lifecycle

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: '' };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: '' };
+    // }
 
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
+
+    // componentDidUpdate() {
+    //     console.log('My component was updated - it rerendered')
+    // }
 
     render() {
 
