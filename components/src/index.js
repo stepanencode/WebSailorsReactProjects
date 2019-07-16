0b7644717d935b3fb4bf03db92d6df3bd2a3c4a3
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-16 12:46:28 +0300

    added props and children

--- components/src/index.js
+++ components/src/index.js
@@ -1,58 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import faker from "faker";
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui containers comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
                 </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Nice blog post!"
+                avatar={faker.image.avatar()}
+            />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 3:00AM"
+                content="I like this subject"
+                avatar={faker.image.avatar()}
+            />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail
+                author="Alex"
+                timeAgo="Yesterday at 5:02PM"
+                content="I like the writing"
+                avatar={faker.image.avatar()}
+            />
+            </ApprovalCard>
         </div>
     )
 };
