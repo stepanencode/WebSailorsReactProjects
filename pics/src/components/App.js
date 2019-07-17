3683ca4b249c7b9bbf440a72c0ae71ce0bb33408
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-17 14:38:31 +0300

    added axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends  React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get(
+            'https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID 3effb0fb53d8423ef4980ae8301ca4362e6523dc9d6a56c980f23fc0debccd79'
+                }
+            }
+        )
+            .then((response) => {
+            console.log(response.data.results);
+        });
     }
 
     render() {
