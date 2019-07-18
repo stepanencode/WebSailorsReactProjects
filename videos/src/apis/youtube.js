b66006573e392e6810e7e8380ab8f285abd0897e
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-18 12:57:13 +0300

    passing state as props

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
-const KEY = 'AIzaSyDtaeGfC-PrrgCrvCv8thmkdd26FAl3VHI';
+import axios from 'axios';
+
+// const KEY = 'AIzaSyDtaeGfC-PrrgCrvCv8thmkdd26FAl3VHI';
+
+export default  axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3'
+    // params: {
+    //     part: 'snippet',
+    //     maxResults: 5,
+    //     key: KEY
+    // }
+});
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from  '../apis/youtube';
+import VideoList from './VideoList';
+
+
+const KEY = 'AIzaSyDtaeGfC-PrrgCrvCv8thmkdd26FAl3VHI';
 
 class App extends  React.Component {
+    state = { videos: [] };
+    onTermSubmit = (term) => {
+        // console.log(term);
+        youtube.get('/search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: KEY,
+                q: term
+            }
+        }).then((response) => {
+            console.log(response);
+            this.setState({
+                videos: response.data.items
+                // selectedVideo: response.data.items[0]
+            });
+        });
+    };
+
     render() {
         return(
             <div className="ui container">
-                <SearchBar />
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
