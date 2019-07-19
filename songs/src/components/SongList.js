4a0caaa2435f2f081d23e88e178302595f1928ce
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-19 13:06:09 +0300

    added render list method

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -4,10 +4,28 @@ import { connect } from 'react-redux';
 class  SongList extends Component {
     // this.props === { songs: state.songs }
 
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            )
+        })
+    };
+
     render() {
-        console.log(this.props);
+        // console.log(this.props);
+        // console.log(this.props.songs);
         return (
-            <div>SongList</div>
+            <div className="ui divided list">{this.renderList()}</div>
         )
     }
 }
