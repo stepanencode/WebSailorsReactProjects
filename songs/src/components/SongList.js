e475450a8d0685a53cea10a0b51530f1a61965e6
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-19 15:18:23 +0300

    added mapStateToProps for SongDetail

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class  SongList extends Component {
     // this.props === { songs: state.songs }
@@ -9,7 +10,10 @@ class  SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
                             Select
                         </button>
                     </div>
@@ -22,7 +26,7 @@ class  SongList extends Component {
     };
 
     render() {
-        // console.log(this.props);
+        console.log(this.props);
         // console.log(this.props.songs);
         return (
             <div className="ui divided list">{this.renderList()}</div>
@@ -31,11 +35,15 @@ class  SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
-    // console.log(state);
+    console.log(state);
     // return state;
     return { songs: state.songs };
 };
+// Don't change the store
+// export default connect(
+//     mapStateToProps, { selectSong }
+// )(SongList);
 
 export default connect(
-    mapStateToProps
-) (SongList);
+    mapStateToProps, { selectSong }
+)(SongList);
