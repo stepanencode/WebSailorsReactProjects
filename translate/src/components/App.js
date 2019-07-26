d837710a865ac6377a0edb7ca63f20484c735ff3
Oksana Stepanenko <stepanencode@gmail.com>
2019-07-26 12:48:12 +0300

    added modified translate

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from "./LanguageSelector";
 
 
 class App extends React.Component {
-    state = { language: 'english' };
-
-    onLanguageChange = language => {
-        this.setState({language})
-    };
 
     render() {
         return (
             <div className='ui container'>
-                <div>
-                    Select a language:
-                    <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
-                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
-                </div>
-                <LanguageContext.Provider value={this.state.language}>
-                    <UserCreate />
-                </LanguageContext.Provider>
+
+                {/*<LanguageContext.Provider value={this.state.language}>*/}
+                {/*    <UserCreate />*/}
+                {/*</LanguageContext.Provider>*/}
+                {/*<LanguageSelector onLanguageChange={this.onLanguageChange}/>*/}
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value="red">
+                        <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
+
+                {/*    <ColorContext.Provider value="red">*/}
+                {/*    <LanguageContext.Provider value={this.state.language}>*/}
+                {/*        <UserCreate />*/}
+                {/*    </LanguageContext.Provider>*/}
+                {/*</ColorContext.Provider>*/}
             </div>
         );
     }
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
     // static contextType = LanguageContext;        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     //         return (
     //             <button className="ui button primary">{text}</button>
     //         )
-    renderSubmit(value) {
-        return value === 'english' ? 'Submit' : 'Voorleggen';
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit' : 'Voorleggen';
+    }
+
+    // render() {
+        // console.log(this.context);
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {({ language }) => this.renderSubmit(language)}
+                </LanguageContext.Consumer>
+            </button>
+        );
     }
 
     render() {
-        // console.log(this.context);
         return (
-            <button className="ui button primary">
-                <LanguageContext.Consumer>
-                    {/*{value => (value === 'english' ? 'Submit' : 'Voorleggen')}*/}
-                    {value => this.renderSubmit(value)}
-                </LanguageContext.Consumer>
-            </button>
+            <ColorContext.Consumer>
+                {color => this.renderButton(color)}
+            </ColorContext.Consumer>
         );
     }
 }
